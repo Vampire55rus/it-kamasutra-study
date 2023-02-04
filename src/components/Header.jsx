e20b17896e0037b6d050f0f5913143c871c3cd4a
Vampire55rus <guz.ser@mail.ru>
2023-02-04 17:14:53 +0600

    Добавление первых компонентов

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import logo from './../logo.svg'
+
+const Header = () => {
+    return (<header className='header'>
+        <img src={logo} />Зоголовок</header>)
+}
+
+export default Header
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+
+
+const Navbar = () => {
+    return (<nav className='nav'>
+        <div><a>Profile</a></div>
+        <div><a>Message</a></div>
+        <div><a>News</a></div>
+        <div><a>Info</a></div>
+    </nav>)
+}
+
+export default Navbar
